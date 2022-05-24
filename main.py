e8dbc3215601472272bdf7b99c1e0d8338ee100e
Leonardo Vitor <61870020+hakstol@users.noreply.github.com>
2022-05-24 14:44:43 -0300

    adicionado comentário de resize

--- main.py
+++ main.py
@@ -31,3 +31,24 @@ def comparar_imagens(nomeImg: str = Form(), img2: UploadFile = File()):
     return {f'{nomeImg} e {img2}'}
     # r2 = requests.get('https://backend-saf-api.azurewebsites.net/Img/imagem2.jpg')
     # img2 = Image.open(BytesIO(r2.content))
+    
+# # Opens a image in RGB mode
+# im = Image.open(r"C:\Users\System-Pc\Desktop\ybear.jpg")
+
+# # Size of the image in pixels (size of original image)
+# # (This is not mandatory)
+# width, height = im.size
+
+# # Setting the points for cropped image
+# left = 4
+# top = height / 5
+# right = 154
+# bottom = 3 * height / 5
+
+# # Cropped image of above dimension
+# # (It will not change original image)
+# im1 = im.crop((left, top, right, bottom))
+# newsize = (300, 300)
+# im1 = im1.resize(newsize)
+# # Shows the image in image viewer
+# im1.show()
